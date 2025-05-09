fab4ef3e8be14ab1df4ebd2304ddea5fa2ea1868
Aritra Bera <beraaritra28@gmail.com>
2025-05-10 01:25:19 +0530

    first commit

--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,9 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import thingspeakImg from "@/public/thingspeak.png";
+import dropboxcloneImg from "@/public/dropboxclone.png";
+import algo_vizgif from "@/public/algo_viz.gif";
 
 export const links = [
   {
@@ -35,56 +35,69 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "Project Intern",
+    location: "Adani Power, Ahmedabad, Gujarat",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
-    icon: React.createElement(LuGraduationCap),
-    date: "2019",
-  },
-  {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
-    description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+      "Coming soon...",
     icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
+    date: "May '25 - Jul '25",
   },
   {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
+    title: "Club Member",
+    location: "Animation and Game Design Club",
     description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: React.createElement(FaReact),
-    date: "2021 - present",
+      "I am a game developer and a member of the Animation and Game Design Club at IIT (ISM) Dhanbad.",
+    icon: React.createElement(CgWorkAlt),
+    date: "Apr '24 - Present",
+  },
+  {
+    title: "Club Member",
+    location: "Electronics and IoT Club",
+    description:
+      "I am a member of the Electronics and IoT Club at IIT (ISM) Dhanbad. I have worked on integrating various sensors with ESP32 and Arduino boards.",
+    icon: React.createElement(CgWorkAlt),
+    date: "Jan '24 - present",
   },
 ] as const;
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "Battery Management System",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+      "This project is an esp32 based IoT battery management system. It uses thingspeak to send data to the cloud.",
+    tags: ["MATLAB", "Simulink", "KiCAD", "LTspice", "Arduino IDE"],
+    imageUrl: thingspeakImg,
   },
   {
-    title: "rmtDev",
+    title: "Dropbox Clone",
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+      "A full-stack web app that allows users to upload files and share them with others. It uses Next.js, Prisma, PostgreSQL and Tailwind.",
+    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Prisma", "PostgreSQL"],
+    imageUrl: dropboxcloneImg,
   },
   {
-    title: "Word Analytics",
+    title: "Pathfinding Algorithm Visualizer",
     description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+      "A unity based pathfinding algorithm visualizer. It uses A* and Dijkstra algorithms to find the shortest path between two points in a maze and visualize it in real time.",
+    tags: ["Unity", "C#"],
+    imageUrl: algo_vizgif,
   },
 ] as const;
 
 export const skillsData = [
+  "C",
+  "C++",
+  "C#",
+  "MATLAB",
+  "Simulink",
+  "KiCAD",
+  "Arduino IDE",
+  "LTspice",
+  "LabVIEW",
+  "Unity",
+  "Python",
+  "TensorFlow",
+  "PyTorch",
   "HTML",
   "CSS",
   "JavaScript",
@@ -95,13 +108,6 @@ export const skillsData = [
   "Git",
   "Tailwind",
   "Prisma",
-  "MongoDB",
-  "Redux",
-  "GraphQL",
-  "Apollo",
   "Express",
   "PostgreSQL",
-  "Python",
-  "Django",
-  "Framer Motion",
 ] as const;
